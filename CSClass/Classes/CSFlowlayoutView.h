b7cad0754af21a4c27a19e1080561d0d5a97d1e0
Mr.s <stqemail@163.com>
2019-02-03 09:56:40 +0800

    add CSFlowlayoutView

---
+++ CSClass/Classes/CSFlowlayoutView.h
@@ -0,0 +1,16 @@
+//
+//  CSFlowlayoutView.h
+//  CSAppComponent
+//
+//  Created by Mr.s on 2019/2/3.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CSFlowlayoutView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CSClass/Classes/CSFlowlayoutView.m
@@ -0,0 +1,20 @@
+//
+//  CSFlowlayoutView.m
+//  CSAppComponent
+//
+//  Created by Mr.s on 2019/2/3.
+//
+
+#import "CSFlowlayoutView.h"
+
+@implementation CSFlowlayoutView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
